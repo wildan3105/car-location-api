e8bc8f2e11c1652cb327350509f55c94615c7133
Wildan <winistein@gmail.com>
2019-07-18 16:04:40 +0700

    Export mockup data as contant variables

--- modules/constants.js
+++ modules/constants.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const data = require('../mockup.json');
+
 module.exports = {
 	defaultPage: 0,
 	defaultItemsInOnePage: 10,
 	defaultSkipItems: 0,
 	defaultFields: ['id','latitude', 'longitude', 'is_on_trip'],
-	defaultSearchRadiusInKm: 5
+	defaultSearchRadiusInKm: 5,
+	data
 };
