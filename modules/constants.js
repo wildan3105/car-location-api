a3f9f58814d845bfc20f28b969deed4062ee8e91
Wildan <winistein@gmail.com>
2019-07-18 11:51:21 +0700

    Add constants/config file

---
+++ modules/constants.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+	defaultPage: 0,
+	defaultItemsInOnePage: 10,
+	defaultSkipItems: 0,
+	defaultFields: ['id','latitude', 'longitude', 'is_on_trip'],
+	defaultSearchRadiusInKm: 5
+};
