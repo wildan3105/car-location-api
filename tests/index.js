df1aba4574ab717ee30e85c5d395ef80ed90d89d
Wildan <winistein@gmail.com>
2019-07-19 21:22:54 +0700

    Add several unit tests

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const expect = require('chai').expect;
 const chaiHttp = require('chai-http');
 const app = require('../index');
 
@@ -7,11 +8,47 @@ chai.should();
 
 describe('Cars endpoint', () => {
 	describe('GET /', () => {
-		it('Should ok with empty content', (done) => {
+		it('Should ok with http code 200 and body is an object', (done) => {
 			chai.request(app)
 				.get('/')
 				.end((err, res) => {
 					res.should.have.status(200);
+					res.body.should.be.a('object');
+					done();
+				})
+		})
+
+		it('Should be not found with http code 404 and body is an object', (done) => {
+			chai.request(app)
+				.get('/someweirdendpoints')
+				.end((err, res) => {
+					res.should.have.status(404);
+					res.body.should.be.a('object');
+					done();
+				})
+		})
+
+		it('Should get all cars', (done) => {
+			chai.request(app)
+				.get('/cars')
+				.end((err, res) => {
+					const data = res.body.data.data;
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					expect(data).to.have.lengthOf.at.least(1);
+					done();
+				})
+		})
+
+		it('Should get a one car by id', (done) => {
+			const id = 8;
+			chai.request(app)
+				.get(`/cars/${id}`)
+				.end((err, res) => {
+					const carById = res.body.carById;
+					res.should.have.status(200);
+					carById.should.be.a('array');
+					expect(carById).to.have.lengthOf(1);
 					done();
 				})
 		})
