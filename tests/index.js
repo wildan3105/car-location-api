af1baf6e874290dc5fa0ab3d771070ebf04e34cd
Wildan <winistein@gmail.com>
2019-07-20 09:22:17 +0700

    API test for pagination

--- tests/index.js
+++ tests/index.js
@@ -7,7 +7,7 @@ chai.use(chaiHttp);
 chai.should();
 
 describe('Cars endpoint', () => {
-	describe('GET /', () => {
+	describe('BASIC ENDPOINTS', () => {
 		it('Should ok with http code 200 and body is an object', (done) => {
 			chai.request(app)
 				.get('/')
@@ -32,7 +32,7 @@ describe('Cars endpoint', () => {
 			chai.request(app)
 				.get('/cars')
 				.end((err, res) => {
-					const data = res.body.data.data;
+					const data = res.body.data;
 					res.should.have.status(200);
 					res.body.should.be.a('object');
 					expect(data).to.have.lengthOf.at.least(1);
@@ -49,6 +49,46 @@ describe('Cars endpoint', () => {
 					res.should.have.status(200);
 					carById.should.be.a('array');
 					expect(carById).to.have.lengthOf(1);
+					done();
+				})
+		})
+
+		it('Should get none car', (done) => {
+			const id = 200;
+			chai.request(app)
+				.get(`/cars/${id}`)
+				.end((err, res) => {
+					res.should.have.status(404);
+					res.body.should.be.a('object');
+					done();
+				})
+		})
+	})
+
+	describe('PAGINATION ENDPOINTS', () => {
+		it('Should return page one (skip = 0 & size = 10)', (done) => {
+			chai.request(app)
+				.get(`/cars`)
+				.end((err, res) => {
+					const data = res.body.data;
+					res.should.have.status(200);
+					res.should.be.a('object');
+					data.should.be.a('array');
+					expect(data).to.have.lengthOf(10);
+					done();
+				})
+		})
+
+		it('Should return data with size = 5', (done) => {
+			const size = 5;
+			chai.request(app)
+				.get(`/cars?size=${size}`)
+				.end((err, res) => {
+					const data = res.body.data;
+					res.should.have.status(200);
+					res.should.be.a('object');
+					data.should.be.a('array');
+					expect(data).to.have.lengthOf(size);
 					done();
 				})
 		})
