25ea2ee5d0be4390225f8f9b7a54db98717ffd10
Wildan <winistein@gmail.com>
2019-07-23 10:16:23 +0700

    Add new function: check if string is a valid JSON string

--- helper/index.js
+++ helper/index.js
@@ -2,6 +2,16 @@ const isObjectEmpty = (obj) => {
 	return Object.keys(obj).length === 0;
 }
 
+const isJson = (string) => {
+	try {
+		let json = JSON.parse(string);
+		return (typeof json == 'object');
+	} catch(e) {
+		return false;
+	}
+}
+
 module.exports = {
-	isObjectEmpty
+	isObjectEmpty,
+	isJson
 };
