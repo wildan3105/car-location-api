f1d1eb85049c3b468c408b9059037a85dd1079e4
Wildan <winistein@gmail.com>
2019-07-23 10:24:48 +0700

    Add http status switch clause

--- endpoints/index.js
+++ endpoints/index.js
@@ -7,11 +7,6 @@ const helper = require('../helper');
 
 const commonFilter = require('../lib');
 
-const { pagination, where, order } = require('../lib/filter');
-
-const orderNames = ['id', 'is_on_trip', 'location_name', 'order_type'];
-const orderTypes = ['asc', 'desc'];
-
 router.get('/', (req, res) => {
 	res.json({
 		...dataStatus,
@@ -21,39 +16,29 @@ router.get('/', (req, res) => {
 
 router.get('/cars', (req, res) => {
 
-	// res.json({
-	// 	...dataStatus,
-	// 	data: commonFilter(req.query)
-	// })
+	const data = commonFilter(req.query);
 
-	if(req.query.from || req.query.size) {
-		res.json({
-			...dataStatus,
-			data: pagination(req.query.from, req.query.size)
-		})
-	} else if(req.query.where) {
-		res.json({
-			...dataStatus,
-			data: where(req.query.where)
-		})
-	} else if(req.query.order_name) {
-		console.log('here');
-		if((orderNames.indexOf(req.query.order_name.toLowerCase()) < 0) || (orderTypes.indexOf(req.query.order_type.toLowerCase())) < 0) {
-			res.status(400).json({
-				status: false,
-				message: `Invalid order_name of '${req.query.order_name}' or order_type of '${req.query.order_type}' `
-			});
-		} else {
-			const { size, data } = order(req.query.order_name.toLowerCase(), req.query.order_type.toLowerCase());
-			res.json({
-				...dataStatus,
-				size,
-				data
-			});
+	if(data.status > 200) {
+		let message;
+		switch(data.status) {
+			case 404: message = 'not found';
+			break;
+
+			case 400: message = 'bad request';
+			break;
+
+			default: message = 'wrong request';
 		}
+
+		res.status(data.status).json({
+			status: false,
+			message
+		})
+		
 	} else {
 		res.json({
-			data: pagination()
+			...dataStatus,
+			data
 		})
 	}
 })
