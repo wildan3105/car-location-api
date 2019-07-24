67bc87e0a5870026a6cdfb07ff87c5300c3d51be
Wildan <winistein@gmail.com>
2019-07-24 12:53:46 +0700

    Assign host in index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.NODE_PORT || 3000;
+const host = process.env.NODE_HOST || '0.0.0.0';
 const routeHandler = require('./endpoints');
 
 app.use('/', routeHandler);
 
-module.exports = app.listen(port, () => {
+module.exports = app.listen(port, host, () => {
 	console.log(`car-location-api is listening on port ${port}`)
 })
